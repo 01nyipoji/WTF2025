ec75467b0799718133df58ca0c2e2bdf8edc9974
Nyipoji Jibarang <nyip2019@gmail.com>
2025-09-05 15:44:18 +0100

    added comments on assignment

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
-let num1 = 20;
-let num2 = 30;
-let name = "Posco";
 
+//declaring variables
+let num1;
+let num2;
+let name;
+
+//assigning values
+num1 = 20;
+num2 = 30;
+name = "Posco";
+
+//performing calculations
 let result = num1 + num2;
 console.log(result);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
 
-let message = "Hello " + name + ", the result of num1 + num2 is " + result;
+//printing message
 
+let message = "Hello " + name + ", the result of num1 + num2 is " + result;
 console.log(message);
